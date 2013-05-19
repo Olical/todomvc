6039043f88760932cc6319f0328576106a4c25e3
Oliver Caldwell <olliec87@gmail.com>
2013-05-19 22:31:32 +0100

    Updated tarmac and added templating.

--- architecture-examples/tarmac/js/app.js
+++ architecture-examples/tarmac/js/app.js
@@ -1,2 +1,18 @@
-define(function () {
+requirejs.config({
+	paths: {
+		text: '../components/requirejs-text/text',
+		Handlebars: '../components/handlebars.js/dist/handlebars',
+		templates: '../templates'
+	},
+	shim: {
+		Handlebars: {
+			exports: 'Handlebars'
+		}
+	}
+});
+
+define([
+	'Handlebars'
+], function (Handlebars) {
+	'use strict';
 });
